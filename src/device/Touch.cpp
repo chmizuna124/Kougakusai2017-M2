9ba863e0cc1fb67047797b4c464e94c35938341b
Usui Kakeru <kakeru3.141592@gmail.com>
2017-04-28 15:24:39 +0900

    namespace 追加

--- src/device/Touch.cpp
+++ src/device/Touch.cpp
@@ -1,66 +1,68 @@
 #include "Touch.h"
 
-//インスタンス変数初期化
-Touch* Touch::_instance = 0;
+namespace device{
+    //インスタンス変数初期化
+    Touch* Touch::_instance = 0;
 
-//シングルトン
-Touch* Touch::getInstance(){
-	if(_instance == 0){
-		_instance = new Touch();
-	}
-	return _instance;
-}
-
-//タッチセンサの状態取得
-bool Touch::isPressed(){
-	return touchSensor.isPressed();
-}
-
-bool Touch::isLongPressed(){
-
-    TimeDetection longTouchDetection = TimeDetection();
-    longTouchDetection.setTargetTime(2000);
-
-    while( 1){
-        if ( isPressed() ){
-            if ( longTouchDetection.isDetected() ){
-                return true;
-            }
+    //シングルトン
+    Touch* Touch::getInstance(){
+        if(_instance == 0){
+            _instance = new Touch();
         }
-        else{
-            return false;
-        }
-        tslp_tsk(20);
+        return _instance;
     }
-    return false;
-}
 
-Touch::State Touch::wait(int maxMs){
-    TimeDetection timeDetection = TimeDetection();
-    timeDetection.setTargetTime(maxMs);
-
-
-    while ( 1 ){
-        if (isPressed() ){
-            if(isLongPressed() ){
-                ev3_speaker_play_tone(700, 300);
-                return LONG_PUSHED;
-            }
-            else {
-                ev3_speaker_play_tone(500, 100);
-                return PUSHED;
-            }
-        }
-        if ( maxMs > 0 && timeDetection.isDetected() ){
-            return NONE;
-        }
-        tslp_tsk(20);
+    //タッチセンサの状態取得
+    bool Touch::isPressed(){
+        return touchSensor.isPressed();
     }
-    return NONE;
-}
 
-//コンストラクタ
-//ポートを指定している
-Touch::Touch()
-	:touchSensor(PORT_1)
-	{}
+    bool Touch::isLongPressed(){
+
+        TimeDetection longTouchDetection = TimeDetection();
+        longTouchDetection.setTargetTime(2000);
+
+        while( 1){
+            if ( isPressed() ){
+                if ( longTouchDetection.isDetected() ){
+                    return true;
+                }
+            }
+            else{
+                return false;
+            }
+            tslp_tsk(20);
+        }
+        return false;
+    }
+
+    Touch::State Touch::wait(int maxMs){
+        TimeDetection timeDetection = TimeDetection();
+        timeDetection.setTargetTime(maxMs);
+
+
+        while ( 1 ){
+            if (isPressed() ){
+                if(isLongPressed() ){
+                    ev3_speaker_play_tone(700, 300);
+                    return LONG_PUSHED;
+                }
+                else {
+                    ev3_speaker_play_tone(500, 100);
+                    return PUSHED;
+                }
+            }
+            if ( maxMs > 0 && timeDetection.isDetected() ){
+                return NONE;
+            }
+            tslp_tsk(20);
+        }
+        return NONE;
+    }
+
+    //コンストラクタ
+    //ポートを指定している
+    Touch::Touch()
+        :touchSensor(PORT_1)
+        {}
+}
--- src/device/Touch.h
+++ src/device/Touch.h
@@ -10,46 +10,48 @@
 using namespace ev3api;
 using namespace detection;
 
-class Touch
-{
-public:
-    enum State{
-        PUSHED,         // タッチセンサが押されたとき
-        LONG_PUSHED,    // 3秒以上おされたとき
-        NONE            // 押されていないとき
+namespace device{
+    class Touch
+    {
+    public:
+        enum State{
+            PUSHED,         // タッチセンサが押されたとき
+            LONG_PUSHED,    // 3秒以上おされたとき
+            NONE            // 押されていないとき
+        };
+
+        static Touch* getInstance();
+    //タッチセンサ
+        /**
+         * タッチセンサ値を返す
+         * 押されていたらtrue
+         * @return タッチセンサ値
+         */
+        bool isPressed();
+
+        /**
+         * タッチセンサ値を返す
+         * 押されていたらtrue
+         * @return タッチセンサ値
+         */
+        bool isLongPressed();
+
+        /**
+         * タッチセンサが押されるまでまつ
+         * @param maxMs 待つ最大の時間 -1 の場合無限（default)
+         * @return State::PUSHED, LONG_PUSHED, NONE
+         */
+        State wait(int maxMs = -1);
+
+    private:
+        //オブジェクトへのポインタ定義
+        TouchSensor    touchSensor;
+
+        static Touch* _instance;
+        //コンストラクタ
+        Touch();
+
     };
-
-	static Touch* getInstance();
-//タッチセンサ
-	/**
-	 * タッチセンサ値を返す
-	 * 押されていたらtrue
-	 * @return タッチセンサ値
-	 */
-	bool isPressed();
-
-	/**
-	 * タッチセンサ値を返す
-	 * 押されていたらtrue
-	 * @return タッチセンサ値
-	 */
-	bool isLongPressed();
-
-    /**
-     * タッチセンサが押されるまでまつ
-     * @param maxMs 待つ最大の時間 -1 の場合無限（default)
-     * @return State::PUSHED, LONG_PUSHED, NONE
-     */
-    State wait(int maxMs = -1);
-
-private:
-	//オブジェクトへのポインタ定義
-	TouchSensor    touchSensor;
-
-	static Touch* _instance;
-	//コンストラクタ
-	Touch();
-
-};
+}
 
 #endif
